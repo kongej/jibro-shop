f92a7e381618b28a75b4f031dda3a517f17de5d2
KCCI119\kcci <kcci@kcci119>
2024-05-19 15:00:19 +0900

    [feat] first setting in sts4

---
+++ shop/src/test/java/com/jibro/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibro.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
