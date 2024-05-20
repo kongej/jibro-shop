6996a04af5342b5a671bf0c2a88425f5fd1331b2
dollyishere <rundollyrun8@gmail.com>
2024-05-20 09:28:50 +0900

    [feat] html 및 dto 제작

--- shop/src/main/java/com/jibro/shop/data/dto/ProductDto.java
+++ shop/src/main/java/com/jibro/shop/data/dto/ProductDto.java
@@ -1,5 +1,41 @@
 package com.jibro.shop.data.dto;
 
-public class ProductDto {
+import java.util.List;
 
+import com.jibro.shop.data.entity.Order;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @author ljy
+ * @since 2024.05.20
+ * Product dto 코드
+ * **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class ProductDto {
+	/* 제품코드(pk) */
+	private String productId;
+	
+	/* 제품명 */
+	private String product;
+	
+	/* 수량 */
+	private Integer productCount = 0;
+	
+	/* 판매가 */
+	private Integer cost;
+	
+	/* 이미지 */
+	private String img;
+	
+	/* order 엔티티와 OneToMany 매핑 */
+	private List<Order> orders;
 }
