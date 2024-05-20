baccbd5c1949aa77ec0fc8ac52e1ee56547250ec
dollyishere <rundollyrun8@gmail.com>
2024-05-20 15:09:11 +0900

    [feat] ProductService 및 필요한 repo 구현

--- shop/src/main/java/com/jibro/shop/data/repository/ProductRepository.java
+++ shop/src/main/java/com/jibro/shop/data/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.jibro.shop.data.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jibro.shop.data.entity.Product;
@@ -10,6 +12,8 @@ import com.jibro.shop.data.entity.Product;
  * 제품 관련 repository 코드
  * **/
 public interface ProductRepository extends JpaRepository<Product, String> {
-
+	
+	// productId 기반으로 상품 조회
+	Optional<Product> findByProductId(String productId);
 }
 
--- shop/src/main/java/com/jibro/shop/service/impl/ProductServiceImpl.java
+++ shop/src/main/java/com/jibro/shop/service/impl/ProductServiceImpl.java
@@ -1,5 +1,7 @@
 package com.jibro.shop.service.impl;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.jibro.shop.data.dto.order.OrderMakeDto;
 import com.jibro.shop.data.dto.product.ProductOrderDto;
 import com.jibro.shop.data.dto.product.ProductResponseDto;
+import com.jibro.shop.data.entity.Product;
 import com.jibro.shop.data.repository.OrderRepository;
 import com.jibro.shop.data.repository.ProductRepository;
 import com.jibro.shop.service.ProductService;
@@ -30,23 +33,48 @@ public class ProductServiceImpl implements ProductService {
 		this.productRepository = productRepository;
 	}
 	
+	// 상품 상세 정보 return
 	@Override
 	public ProductResponseDto getProduct(String productId) {
-		// TODO Auto-generated method stub
-		return null;
+		LOGGER.info("[getProduct] input productId : {}", productId);
+		Optional<Product> product = productRepository.findByProductId(productId);
+		
+		ProductResponseDto productResponseDto = new ProductResponseDto();
+		
+		// 해당 product 존재 시, 해당 값 ProductResponseDto에 넣어서 보내주기
+		if (product.isPresent()) {
+			LOGGER.info("[getProduct] product number : {}, name : {}", product.get().getProductId(),
+		            product.get().getProduct());
+			productResponseDto.setProductId(product.get().getProductId());
+			productResponseDto.setProduct(product.get().getProduct());
+			productResponseDto.setProductCount(product.get().getProductCount());
+			productResponseDto.setImg(product.get().getImg());
+			productResponseDto.setCost(product.get().getCost());
+		}
+		
+		return productResponseDto;
 	}
 
+	// 주문 위한 상품 상세 정보 & 이전 페이지에서 건네받은 상품 선택 개수 ProductOrderDto 형태로 합쳐서 return
 	@Override
 	public ProductOrderDto makeOrder(OrderMakeDto orderMakeDto) {
 		
-		ProductResponseDto productResponseDto = new ProductResponseDto();
+		LOGGER.info("[makeOrder] input orderMakeDto productId : {}, selectedCount : {}", 
+				orderMakeDto.getProductId(), orderMakeDto.getSelectedCount());
+		Optional<Product> product = productRepository.findByProductId(orderMakeDto.getProductId());
+		
 		ProductOrderDto productOrderDto = new ProductOrderDto();
 		
-		productOrderDto.setProductId(productResponseDto.getProductId());
-		productOrderDto.setCost(productResponseDto.getCost());
-		productOrderDto.setProduct(productResponseDto.getProduct());
-		productOrderDto.setImg(productResponseDto.getImg());
-		productOrderDto.setSelectedCount(orderMakeDto.getSelectedCount());
+		// 해당 product 존재 시, 해당 값 ProductResponseDto에 넣어서 보내주기
+		if (product.isPresent()) {
+			LOGGER.info("[makeOrder] product number : {}, name : {}", product.get().getProductId(),
+		            product.get().getProduct());
+			productOrderDto.setProductId(product.get().getProductId());
+			productOrderDto.setProduct(product.get().getProduct());
+			productOrderDto.setImg(product.get().getImg());
+			productOrderDto.setCost(product.get().getCost());
+			productOrderDto.setSelectedCount(orderMakeDto.getSelectedCount());
+		}
 		
 		return productOrderDto;
 	}
