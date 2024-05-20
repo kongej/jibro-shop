2729962626e38e20edc0e5a121f2f6c7fc963bed
dollyishere <rundollyrun8@gmail.com>
2024-05-20 10:55:29 +0900

    [feat] controller 초기 설정

--- shop/src/main/java/com/jibro/shop/controller/OrderController.java
+++ shop/src/main/java/com/jibro/shop/controller/OrderController.java
@@ -1,5 +1,41 @@
 package com.jibro.shop.controller;
 
-public class OrderController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.jibro.shop.service.OrderService;
+
+/**
+ * @author ljy
+ * @since 2024.05.20
+ * Order Controller 코드
+ * Shop 내부용 컨트롤러 (외부 api 제공 x)
+ * **/
+@Controller
+public class OrderController {
+	
+	// 의존성 주입(OrderService)
+	@Autowired
+	private OrderService orderService;
+
+	@PostMapping("/order/create")
+	public String createOrder() {
+		
+		return "redirect:/order/check";
+	}
+	
+	@GetMapping("/order/check")
+	public ModelAndView checkOrder() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
+	
+	@PostMapping("/order/detail")
+	public ModelAndView getOrder() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 }
--- shop/src/main/java/com/jibro/shop/controller/ProductController.java
+++ shop/src/main/java/com/jibro/shop/controller/ProductController.java
@@ -1,5 +1,37 @@
 package com.jibro.shop.controller;
 
-public class ProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.jibro.shop.service.ProductService;
+
+/**
+ * @author ljy
+ * @since 2024.05.20
+ * Product Controller 코드
+ * Shop 내부용 컨트롤러 (외부 api 제공 x)
+ * **/
+@Controller
+public class ProductController {
+	
+	// 의존성 주입(ProductService)
+	@Autowired
+	private ProductService productService;
+	
+	// product 상세 화면으로 이동
+	@GetMapping("/product/{productId}")
+	public ModelAndView getProduct() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
+	
+	// order 신규 등록 화면으로 이동
+	@PostMapping("/order/make")
+	public ModelAndView makeOrder() {
+		ModelAndView mav = new ModelAndView();
+		return mav;
+	}
 }
