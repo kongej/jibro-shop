b6fb3b785e838431a0ca9121932fcbae270b5d0b
dollyishere <rundollyrun8@gmail.com>
2024-05-20 10:23:20 +0900

    [feat] service 초기 세팅

--- shop/src/main/java/com/jibro/shop/service/ProductService.java
+++ shop/src/main/java/com/jibro/shop/service/ProductService.java
@@ -1,5 +1,15 @@
 package com.jibro.shop.service;
 
+import com.jibro.shop.data.dto.ProductDto;
+
+/**
+ * @author ljy
+ * @since 2024.05.20
+ * Product Service 기조 코드
+ * **/
 public interface ProductService {
+	
+	/* 제품 상세 정보 제공 */
+	ProductDto getProduct(String productId);
 
 }
